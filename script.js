514ac18afe6cd09d5054f2ff8723de62e1255268
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-05-15 22:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// Creating the student object
+var student = {
+  name: "John Doe"
+};
+
+// Adding getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Using getKeys() method on the student object
+var keys = student.getKeys();
+
+// Printing the keys
+console.log(keys);
+
+console.log(student.getKeys());
